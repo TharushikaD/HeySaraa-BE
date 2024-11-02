469b57c938b894de510ba6f04f1cc590f4f03eaa
Tharushika Dilmini <dilz2001.1221@gmail.com>
2024-11-02 07:22:11 +0530

    commit

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Top Up\Final Project\Application\BackEnd\env
+set VIRTUAL_ENV=D:\Top Up\Final Project\Application\HeySaraa-BE\env
 
 if not defined PROMPT set PROMPT=$P$G
 
